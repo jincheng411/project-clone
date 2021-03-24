6d1640d5edc458a8f24da7a58560d9977467a9c0
jincheng411 <jincheng411@gmail.com>
2021-03-24 17:20:39 -0400

    add hover for order text

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 function Product({ name, imgUrl }) {
+  const handleOnMouseEnter = () => {
+    document.getElementById(`order-${imgUrl}`).style.visibility = 'visible';
+  }
+  const handleOnMouseLeave = () => {
+    document.getElementById(`order-${imgUrl}`).style.visibility = 'hidden';
+  }
   return (
-    <div className="menu-product">
-      <img src={process.env.PUBLIC_URL + imgUrl} alt='product' />
+    <div className="menu-product" onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave}>
+      <img src={process.env.PUBLIC_URL + imgUrl} alt="product" />
       <div className="menu-title">
         <h4>{name}</h4>
+      </div>
+      <div id={`order-${imgUrl}`}>
+        <span>ORDER</span>
+        <span className="order-arrow"><ArrowForwardIcon /></span>
       </div>
     </div>
   );
