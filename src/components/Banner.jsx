a85ee3383d90f3385b7218014bae66ea171f5c00
jincheng411 <jincheng411@gmail.com>
2021-03-25 11:54:15 -0400

    set background video size properly

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
+import './Banner.css';
 
 function Banner() {
   return (
     <div className="banner">
-      <div className="video-banner">
-        <video src="https://www.chipotle.com/content/dam/poc/order/images/promo-banners/qd/v3-2300x520-Quesadilla-R7.mp4" />
-      </div>
+      <video autoplay="autoplay" loop="loop" muted="muted" playsinline webkit-playsinline className="video-banner" src="https://www.chipotle.com/content/dam/poc/order/images/promo-banners/qd/v3-2300x520-Quesadilla-R7.mp4" />
       <div className="promo">
         <div className="promo-text-line1">
-          <span>new</span>
-          <p>hand-crafted</p>
-        </div>
-        <div className="promo-text-line2">
-          <p>quesadilla</p>
-        </div>
-        <div className="promo-text-line3">
-          <p>-only available online</p>
-        </div>
-        <div className="promo-text-line4">
           <p>
             A whole new way to Chipotle paired with
             your favorite salsas and sides for pickup or delivery.
           </p>
         </div>
         <div className="promo-btn"><button>ORDER NOW</button></div>
-        <div className="promo-text-line5">
+        <div className="promo-text-line2">
           <p>Not available to order in-restaurant.</p>
         </div>
       </div>
