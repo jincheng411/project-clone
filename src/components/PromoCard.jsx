9f346d2f61db32f0e6b04210fbed8b71dc12d006
jincheng411 <jincheng411@gmail.com>
2021-03-26 16:56:31 -0400

    fixed image layout

--- src/components/PromoCard.jsx
+++ src/components/PromoCard.jsx
@@ -1,12 +1,19 @@
-// import React from 'react';
-// import './PromoCard.css';
+import React from 'react';
+import './PromoCard.css';
 
-// function PromoCard({ title, description, url }) {
-//   return (
-//     <div className="promo-cards">
-//       {/* <div>{title}</div> */}
-//     </div>
-//   );
-// }
+function PromoCard({ title, description, url }) {
+  return (
+    <div className="promo-card">
+      <div className="promo-card-left">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <span>order now</span>
+      </div>
+      <div className="promo-card-right">
+        <img src={url} alt="promo" />
+      </div>
+    </div>
+  );
+}
 
-// export default PromoCard;
+export default PromoCard;
