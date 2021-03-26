50d14c68f9ab6e58f3135cf733b50223dc7a7992
jincheng411 <jincheng411@gmail.com>
2021-03-26 15:56:57 -0400

    fixed eslint error

---
+++ src/components/PromoCard.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import './PromoCard.css';
+
+// function PromoCard({ title, description, url }) {
+//   return (
+//     <div className="promo-cards">
+//       {/* <div>{title}</div> */}
+//     </div>
+//   );
+// }
+
+// export default PromoCard;
