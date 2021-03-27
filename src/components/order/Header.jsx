4ae2e8fb99a17543b0e65e9428f4c2cf62300bec
jincheng411 <jincheng411@gmail.com>
2021-03-27 09:39:45 -0400

    finish header

--- src/components/order/Header.jsx
+++ src/components/order/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Header.css'
 function Header({ product }) {
   return (
-    <div className="header">
+    <div className="order-header">
       <div className="header-left">
         <img src={product.url} />
       </div>
