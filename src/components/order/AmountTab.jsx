81d97659a9b569d7e1755af4bebf704d4c55341c
jincheng411 <jincheng411@gmail.com>
2021-04-08 15:52:07 -0400

    finished side data

--- src/components/order/AmountTab.jsx
+++ src/components/order/AmountTab.jsx
@@ -5,7 +5,7 @@ import './AmountTab.css';
 function AmountTab({ amount, quantity, isVisible, hideAmount }) {
   return (
     <div className={`amount-tab ${isVisible && "tab-visible"}`} onClick={hideAmount} >
-      {(amount === '1') && <span><CheckIcon style={{'font-size': '2rem', 'font-weight': '800'}} /></span>}
+      {(amount === '1') && <span><CheckIcon fontWeight="fontWeightBold" style={{'font-size': '2rem'}} /></span>}
       {(amount &&  amount!== '1') && <span>{amount}</span>}
       {quantity && <span>{quantity}</span>}
     </div>
