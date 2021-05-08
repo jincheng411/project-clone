203244f0dc16b5678ff545e718a40304e786bc97
jincheng411 <jincheng411@gmail.com>
2021-05-07 23:50:13 -0400

    get section data from database

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -5,21 +5,33 @@ import Header from './order/Header.jsx';
 import Section from './order/Section.jsx';
 import AddToBag from './order/AddToBag.jsx';
 import {products} from '../menu.js';
+import axios from 'axios';
 
 function Order({products}) {
   const product = products[0];
   const [sections, setSections] = useState([]);
+  useEffect(() => {
+    axios.get('/api/sections')
+    .then((res) => {
+      console.log(res)
+      setSections(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [setSections])
+  console.log(sections)
   return (
     <>
       <div className="order">
         <Header product={product} />
-        {/* <Section section={product.sections[0]} category='protein' />
-        <Section section={product.sections[1]} category='rice' />
-        <Section section={product.sections[2]} category='beans' />
-        <Section section={product.sections[3]} category='topping' />
-        <Section section={product.sections[4]} category='options' />
-        <Section section={product.sections[5]} category='side' />
-        <Section section={product.sections[6]} category='drinks' /> */}
+        <Section section={sections[0]} category='protein' />
+        <Section section={sections[1]} category='rice' />
+        <Section section={sections[2]} category='beans' />
+        <Section section={sections[3]} category='topping' />
+        <Section section={sections[4]} category='options' />
+        <Section section={sections[5]} category='side' />
+        <Section section={sections[6]} category='drinks' />
       </div>
       <AddToBag />
     </>
